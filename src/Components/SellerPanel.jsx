fd00c15c4fb2ee283fe4e636ed544936f32764d1
Sujal Rajput <rajputsujal992@gmail.com>
2023-09-18 16:07:24 +0530

    login and sign up form work is in progress

--- src/Components/SellerPanel.jsx
+++ src/Components/SellerPanel.jsx
@@ -1,10 +1,46 @@
-import React from 'react'
-import Login from './Login'
+import React from "react";
+import Login from "./Login";
+import Signup from "./Signup";
+import { Link } from "react-router-dom";
 
 export default function SellerPanel() {
   return (
     <>
-    <Login />
+      {/* header */}
+      <div className="container pt-3">
+        <div className="row justify-content-center">
+          <div className="col col-12">
+            <h3
+              className="text-center"
+              style={{ color: "#5c0431", fontSize: "2rem" }}
+            >
+              Your Panel
+            </h3>
+          </div>
+        </div>
+      </div>
+      <div className="container sellerPanel mt-5">
+        <div className="row d-flex border-top border-bottom py-4">
+          <h4 className="h4 col-5">Shop Name</h4>
+          <h5 className="h4 col-6">Owner's Name</h5>
+          <Link className="col-1 btn btn-outline-secondary">Edit Details</Link>
+        </div>
+        <div className="row d-flex mt-3 border-bottom py-4">
+          <h4 className="h3 col-10">Your Products</h4>
+          <Link className="col-2 btn btn-primary">Add a new Product</Link>
+        </div>
+        <div className="row gap-2 d-flex mt-3 border-bottom py-4">
+          <div className="col-1">
+            <img src="" alt="sads" className="w-75" />
+          </div>
+          <h4 className="col-8 h4">Shining Sareee</h4>
+          <Link className="btn col-1 btn-outline-warning ">Edit</Link>
+          <Link className="btn  col-1 btn-outline-danger">Delete</Link>
+        </div>
+      </div>
+
+      {/* <Signup />
+    <Login /> */}
     </>
-  )
+  );
 }
--- src/Components/Signup.jsx
+++ src/Components/Signup.jsx
@@ -1,9 +1,187 @@
-import React from "react";
+import React, { useState } from "react";
 
 export default function Signup() {
-  try {
-    return <div>Signup</div>;
-  } catch (error) {
-    return <>error</>;
-  }
+  const initialSignup = {
+    ownerName: "",
+    shopName: "",
+    sellerEmail: "",
+    password: "",
+  };
+
+  const [signupData, setSignupData] = useState(initialSignup);
+  const { ownerName, shopName, sellerEmail, password } = signupData;
+
+  //for first password to check confirm passwrd
+  const [password1, setPassword1] = useState("");
+
+  //errors
+  const [emptyOwnerNameError, setEmptyOwnerNameError] = useState(false);
+  const [emptyShopNameError, setEmptyShopNameError] = useState(false);
+  const [emptyEmailError, setEmptyEmailError] = useState(false);
+  const [emptyPasswordError, setEmptyPasswordError] = useState(false);
+  const [pswrdDosntMatching, setPswrdDoesntMatching] = useState(false);
+
+  const inputChange = (e) => {
+    e.preventDefault();
+
+    setSignupData({
+      ...signupData,
+      [e.target.name]: e.target.value,
+    });
+  };
+  const SignUp = (e) => {
+    e.preventDefault();
+
+    if (ownerName.length > 0) {
+      if (shopName.length > 0) {
+        if (
+          sellerEmail.length > 0 &&
+          sellerEmail.includes(".") &&
+          sellerEmail.includes("@")
+        ) {
+          if (password1.length > 8 || password1.length === 8) {
+            if (password1 === password) {
+              // ---------
+              console.log(signupData);
+            } else {
+              setPswrdDoesntMatching(true);
+            }
+          } else {
+            setEmptyPasswordError(true);
+          }
+        } else {
+          setEmptyEmailError(true);
+        }
+      } else {
+        setEmptyShopNameError(true);
+      }
+    } else {
+      setEmptyOwnerNameError(true);
+    }
+  };
+
+  return (
+    <>
+      {/* header */}
+      <div className="container pt-3">
+        <div className="row justify-content-center">
+          <div className="col col-12">
+            <h3
+              className="text-center"
+              style={{ color: "#5c0431", fontSize: "2rem" }}
+            >
+              Sign Up
+            </h3>
+          </div>
+        </div>
+      </div>
+
+      {/* form */}
+      <form className="container mt-5 pt-5" onSubmit={SignUp}>
+        <div className="row d-flex justify-content-center">
+          <div className="mb-3 col-4 ">
+            <input
+              type="text"
+              value={ownerName}
+              onChange={inputChange}onInput={()=>{
+                setEmptyOwnerNameError(false);
+              }}
+              name="ownerName"
+              className="form-control"
+              placeholder="Enter Shop's Owner Name"
+            />
+            {emptyOwnerNameError && (
+              <p className="text-danger">Please enter your Owner's Name</p>
+            )}
+          </div>
+        </div>
+        <div className="row d-flex justify-content-center">
+          <div className="mb-3 col-4 ">
+            <input
+              type="text"
+              value={shopName}
+              onChange={inputChange}onInput={()=>{
+                setEmptyShopNameError(false);
+              }}
+              name="shopName"
+              className="form-control"
+              placeholder="Enter Shop Name"
+            />
+            {emptyShopNameError && (
+              <p className="text-danger">Please enter your Shop's Name</p>
+            )}
+          </div>
+        </div>
+        <div className="row d-flex justify-content-center">
+          <div className="mb-3 col-4 ">
+            <input
+              type="email"
+              value={sellerEmail}
+              onChange={inputChange}onInput={()=>{
+                setEmptyEmailError(false);
+              }}
+              name="sellerEmail"
+              className="form-control"
+              placeholder="Enter Your Email Address"
+            />
+            {emptyEmailError && (
+              <p className="text-danger">Please enter a Valid Email Address</p>
+            )}
+          </div>
+        </div>
+        <div className="row d-flex justify-content-center">
+          <div className="mb-3 col-4 ">
+            <input
+              type="password"
+              onChange={(e) => {
+                setPassword1(e.target.value);
+              }}onInput={()=>{
+                setEmptyPasswordError(false);
+              }}
+              className="form-control"
+              placeholder="Enter Your Password"
+            />
+            {emptyPasswordError && (
+              <p className="text-danger">
+                Password Should have minimum 8 letters
+              </p>
+            )}
+          </div>
+        </div>
+        <div className="row d-flex justify-content-center">
+          <div className="mb-3 col-4 ">
+            <input
+              type="password"
+              value={password}
+              onChange={inputChange}onInput={()=>{
+                setPswrdDoesntMatching(false);
+              }}
+              name="password"
+              className="form-control"
+              placeholder="Confirm Your Password"
+            />
+            {pswrdDosntMatching && (
+              <p className="text-danger">
+                Password are Not matching, Please enter a password you entered
+                first
+              </p>
+            )}
+          </div>
+        </div>
+        <div className="row d-flex justify-content-center">
+          <div className="mb-3 col-2 mt-5">
+            <button type="submit" className="btn btn-success w-100">
+              Sign Up
+            </button>
+          </div>
+        </div>
+      </form>
+    </>
+  );
 }
+
+// try {
+//   return <div>Signup</div>;
+// } catch (error) {
+//   return <>error</>;
+// }
